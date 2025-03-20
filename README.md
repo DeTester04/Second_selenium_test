9acd2442259563b648279804bd9fe6920fa2a186
4ofDECEMBER@1990 <nnennaokibe@gmail.com>
2025-03-20 13:51:20 +0100

    selenium_Test_II

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Second_selenium_test
+
+import time → Allows us to use time.sleep() to pause execution.
+from selenium import webdriver → Loads Selenium's WebDriver, which lets us control a browser.
+from selenium.webdriver.common.by import By → Helps us locate elements on the webpage.
--- Selenium_test II.py
+++ Selenium_test II.py
@@ -6,32 +6,38 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.saucedemo.com/inventory.html") # Test URL
-time.sleep(5)
+# Variables
+username = "visual_user"
+password = "secret_sauce"
+url = "https://www.saucedemo.com/inventory.html"
+pause = 5  # Pause time in seconds
+
+driver.get(url) # Test URL
+time.sleep(pause)
 
 #Log in page
-driver.find_element(By.ID, "user-name").send_keys("visual_user")# UserName text field
-time.sleep(5)
-driver.find_element(By.ID, "password").send_keys("secret_sauce")# Password text field
-time.sleep(5)
+driver.find_element(By.ID, "user-name").send_keys(username)# UserName text field
+time.sleep(pause)
+driver.find_element(By.ID, "password").send_keys(password)# Password text field
+time.sleep(pause)
 driver.find_element(By.ID, "login-button").click()# Click Log in Button
 
 # product page
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()# Click Add to Cart Button
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()# Click Add to Cart Button
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()# Click Add to Cart Button
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()# Click Add to Cart Button
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()# Click Add to Cart Button
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()# Click Add to Cart Button
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "react-burger-menu-btn").click()# Click hamburger menu
-time.sleep(5)
+time.sleep(pause)
 driver.find_element(By.ID, "logout_sidebar_link").click()# Click log out button
-time.sleep(5)
+time.sleep(pause)
 
